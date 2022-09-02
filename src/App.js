c7b232ad226acb99b7c30e08f0d8f43a54fe8e98
dougiehawes <dougiehawes@hotmail.com>
2022-09-02 20:08:52 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 
-function App() {
+import "./style.css";
+
+const App = () => {
+  const [menuOpen, toggleMenu] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app full-screen">
+      <BrowserRouter>
+        <header className="header">
+          <h1 className="title">
+            <Link to="/">Doug's Design</Link>
+          </h1>
+          <div className="menu-container" onClick={() => toggleMenu(!menuOpen)}>
+            <div className={`menu-icon ${menuOpen && "menu-icon-open"}`}>
+              <div className="menu-bar bar1"></div>
+              <div className="menu-bar bar2"></div>
+              <div className="menu-bar bar3"></div>
+            </div>
+            <div className={`menu-body ${menuOpen && "menu-body-open"}`}>
+              <h2 className="menu-title">EFFECTS</h2>
+              <Link to="/effect1">Effect1</Link>
+              <h2 className="menu-title">UTILITIES</h2>
+              <Link to="/buttons">Buttons</Link>
+            </div>
+          </div>
+        </header>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
